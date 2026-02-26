ab56d0e309c6c561905db65fd59e630eccee4f21
solanaf <sbfernandez@g.ucla.edu>
2026-02-26 15:41:57 -0800

    max_chop & include_PHI flags

--- deidentify/deidentify.py
+++ deidentify/deidentify.py
@@ -9,7 +9,9 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--file", type=str, default="../data/EEG Notes 12_23_2025.xlsx", help="Input spreadsheet file name")
 parser.add_argument("-n","--num_batch", type=int, default=4, help="Input number of batches")
-parser.add_argument("--include_PHI",action='store_true', help='Keep PHI')
+parser.add_argument("--include_PHI",action='store_true', help='Keep PHI. Includes full length of note, and does not run Philer-UCSF')
+parser.add_argument("--max_chop",action='store_true', help="Reduce the clinical note to just the impressions section (shortest length possible), if present. Can be run with --include_PHI or default setting that runs Philter-UCSF.")
+parser.add_argument("--time_filter",action='store_true', help="Only include records from 2/12/24-2/12/26")
 args = parser.parse_args()
 
 #### CHECK IF PHILTER-UCSF IS AVAILABLE
@@ -52,23 +54,26 @@ df.columns = df.columns.str.lower()
 df["note_text"] = df["note_text"].astype(str)
 df["note_id"] = df["note_id"].astype(str)
 
-df_sorted = df.sort_values(by=["note_id", "line"])
-
-# columns = ["note_id", "line", "spec_time_loc_dttm", "note_status", "note_type", "date_of_service_dttm",
-#            "encounter_type", "pat_name", "mrn", "birth_date", "loc_name", "ethnicity", "race",
-#            "ethnic_background", "gender_identity", "problem_list", "department_name", "serv_area_id",
-#            "room", "hosp_admsn_time", "hosp_disch_time", "inpatient_days", "discharge_location",
-#            "cpt_codes", "note_text"]
+if args.time_filter:
+    df["spec_time_loc_dttm"] = pd.to_datetime(df["spec_time_loc_dttm"])
+    df = df[df["spec_time_loc_dttm"].between(pd.Timestamp('2024-02-12'), pd.Timestamp('2026-02-12'))]
 
 non_eeg_count  = 0
 
 def process_note(g):
+    """
+    Main function that collapses multi-line notes and removes the PHI header, 
+    or maximally chops down to the "impressions" section, if present.
+    """
     global non_eeg_count
 
-    # collapse multi-line notes
-    row = g.iloc[0].copy()
-    row["line"] = g["line"].max()
-    full_text = " ".join(g["note_text"].dropna().astype(str))
+    if "line" in df.columns:
+        #collapse multi-line notes
+        row = g.iloc[0].copy()
+        row["line"] = g["line"].max()
+        full_text = " ".join(g["note_text"].dropna().astype(str))
+    else:
+        full_text = g
 
     # Remove rows that are not EEG records (e.g., lumbar punctures)
     if "EEG" not in full_text:
@@ -80,31 +85,54 @@ def process_note(g):
         non_eeg_count += 1
         return None
 
-    # check for starter words
-    if not args.include_PHI:
+    # check for starter words if max chopping or only removing header
+    if args.max_chop:
+        WORDS = ["summary",
+                 "summary of findings",
+                 "impression",
+                 "findings",
+                 "technique",
+                 "recording method",
+                 "conditions of the recording"]
+        for w in WORDS:
+            start = full_text.lower().find(w)
+            if start >= 0:
+                full_text = full_text[start:]
+                break
+    elif not args.include_PHI:
         WORDS = ["conditions of the recording", 
                 "recording method", 
                 "technique",
                 "findings",
                 "impression",
                 "summary of findings",
-                "summary"]
+                "summary"]   
         for w in WORDS:
             start = full_text.lower().find(w)
             if start >= 0:
                 full_text = full_text[start:]
                 break
-    
-    row["note_text"] = full_text.strip()
-    return row
 
-df_merged = (
+    if "line" in df.columns:
+        row["note_text"] = full_text.strip()
+        return row   
+    else:
+        return full_text.strip()
+
+if "line" in df.columns:
+    df_sorted = df.sort_values(by=["note_id", "line"])
+    df_merged = (
     df_sorted
     .groupby("note_id", as_index=False)
     .apply(process_note, include_groups=False)
     .dropna(subset=["note_text"])
     .reset_index(drop=True)
-)
+    )
+else:
+    df_merged = df
+    df_merged["note_text"] = df["note_text"].apply(process_note)
+    df_merged.dropna(subset=["note_text"]).reset_index(drop=True)   
+
 
 records = df_merged.shape[0]
 
